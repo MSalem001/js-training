e2e2ea55ea295b42329b0f99d293b43e533aa68c
Mustafa Salem <Mustafa@gmail.com>
2025-08-11 23:02:47 +0300

    Creatin javascript file and css file

---
+++ script.js
@@ -0,0 +1,11 @@
+//DOM
+//attributes
+let element = document.getElementById("second");
+console .log(element.previousElementSibling);
+console .log(element.nextElementSibling);
+element.nextElementSibling.style.color = "red";
+element.previousElementSibling.style.color = "blue";
+element.style.color="white";
+element.style.backgroundColor = "black";
+element.style.padding = "10px";
+element.parentElement
